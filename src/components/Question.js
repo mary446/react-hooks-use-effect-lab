a3aac5beae3e3366dd1bc2ed17e9b75c4377a4eb
mary446 <135317707+mary446@users.noreply.github.com>
2023-07-20 20:45:30 +0300

    initial commit

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,67 @@
-import React, { useState } from "react";
+// import React, { useEffect, useState } from "react";
+
+// function Question({ question, onAnswered }) {
+//   const [timeRemaining, setTimeRemaining] = useState(10);
+  
+//   // add useEffect code
+//   // useEffect(() => {
+//   //   setTimeout((1000) => 
+//   //   );
+//   // }) 
+//   useEffect( ()=> {
+//     const timeRemaining= setTimeout-1;
+//     const interval = setInterval(()=>{
+//       setTimeRemaining(timeRemaining);
+//       setInterval(timeRemaining);
+//     }, 1000);
+
+//     return () => clearInterval(interval);
+// },[]);
+
+//   function handleAnswer(isCorrect) {
+//     setTimeRemaining(10);
+//     onAnswered(isCorrect);
+//   }
+
+//   const { id, prompt, answers, correctIndex } = question;
+
+//   return (
+//     <>
+//       <h1>Question {id}</h1>
+//       <h3>{prompt}</h3>
+//       {answers.map((answer, index) => {
+//         const isCorrect = index === correctIndex;
+//         return (
+//           <button key={answer} onClick={() => handleAnswer(isCorrect)}>
+//             {answer}
+//           </button>
+//         );
+//       })}
+//       <h5>{timeRemaining} seconds remaining</h5>
+//     </>
+//   );
+// }
+
+// export default Question;
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(()=>{
+    if(timeRemaining === 0 ){
+      setTimeRemaining(10)
+      onAnswered(false)
+      return;
+    }
+const timeId = setTimeout(()=>{
+  setTimeRemaining(timeRemaining - 1  )
+},1000)
+return function () {
+  clearTimeout(timeId)
+};
+  },[timeRemaining,onAnswered])
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
